42f81badc2cea9e5ada3a1707f97f2065e3fcbe3
gitBugs <bugs@binny.eu>
2016-04-19 21:56:37 +0100

    Uploaded Genuino Day 2016 files

---
+++ 2016 Genuino Day/Piezo_Example/Piezo_Example.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9, OUTPUT); //We're going to use Pin 9 as an output
+  Serial.begin(9600); //We're going to send data over the Serial connection at baud rate 9600
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // read the sensor:
+  int sensorReading = analogRead(A0);
+  int loopDelay = sensorReading / 10 - 3
+
+  // print the delay factor we've calculated
+  Serial.println(loopDelay);
+
+  //Turn the pin on, wait for the correct time
+  digitalWrite(9, HIGH);
+  delay(loopDelay);
+  
+  //...Then turn it back off, and wait again
+  digitalWrite(9, LOW);
+  delay(loopDelay);
+
+  //...now the loop has finished, we're going back to the start  
+}
---
+++ 2016 Genuino Day/Servo_example/Servo_example.ino
@@ -0,0 +1,28 @@
+#include <Servo.h> //This is the library we're using; 
+                   //a pre-written chunk of code to make 
+                   //a complex task much easier
+
+Servo myservo;     // create servo "object" to control a servo
+
+int sensorPin = 0; // analog pin used to connect the light dependant resistor
+int sensorValue;   // variable to read the value from the analog pin
+int servoValue;    // variable to hold the value that we'll send to the Servo
+
+void setup() {
+  myservo.attach(9);  // Tells the library that we're going to attach the servo to Pin 9
+  Serial.begin(9600); // Get ready to send data over the Serial connection to the computer
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);   // reads the value of the LDR (value between 0 and 1023)
+  
+  servoValue = map(sensorValue, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(servoValue);                  // sets the servo position according to the scaled value
+  
+  Serial.print(sensorValue);             // Print the sensor value
+  Serial.print(" -> ");                // ...and a little arrow
+  Serial.println(servoValue);            // ...and the value we've just sent to the servo
+  
+  delay(15);                           // waits for the servo to get there
+}
+
